2d82044dd0090ef87d08c04072603284e109870b
tmfdk333 <tmfdk333@gmail.com>
2019-03-12 23:19:48 +0900

    2.3.1.1 GET 요청 처리

---
+++ ch02/src/main/java/info/thecodinglive/basic/HelloServlet.java
@@ -0,0 +1,24 @@
+package info.thecodinglive.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="HelloServlet", urlPatterns={"/helloget"})
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doGet 메소드 호출");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter writer = response.getWriter();
+
+        response.setContentType("text/html");
+        writer.println("<html>");
+        writer.println("<head>jpub java webservice</head>");
+        writer.println("<body> get 요청 예제입니다.. </body");
+        writer.println("</html>");
+    }
+}
