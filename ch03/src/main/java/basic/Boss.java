84274c5ba30d468d0bef336bed00c8b4374d7655
tmfdk333 <tmfdk333@gmail.com>
2019-03-13 22:23:33 +0900

    3.2.1 인터페이스와 스프링

---
+++ ch03/src/main/java/basic/Boss.java
@@ -0,0 +1,8 @@
+package basic;
+
+public class Boss implements WorkManager {
+    @Override
+    public String doIt() {
+        return "do Boss";
+    }
+}
---
+++ ch03/src/main/java/basic/Employee.java
@@ -0,0 +1,8 @@
+package basic;
+
+public class Employee implements WorkManager {
+    @Override
+    public String doIt() {
+        return "do Work";
+    }
+}
