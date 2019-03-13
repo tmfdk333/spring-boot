5da323ca1fc3c8c34f4ba65e99412bac1d65ffe4
tmfdk333 <tmfdk333@gmail.com>
2019-03-13 22:53:05 +0900

    3.2.3.3 어노테이션 설정 시 생명주기 제어

--- ch03/src/main/java/basic/Employee.java
+++ ch03/src/main/java/basic/Employee.java
@@ -1,13 +1,20 @@
 package basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class Employee implements WorkManager {
     @Override
     public String doIt() {
         return "do Work";
     }
+
+    @PostConstruct
     public void onCreated() {
         System.out.println("Employee 생성");
     }
+
+    @PreDestroy
     public void onDestroyed() {
         System.out.println("Employee 소멸");
     }
