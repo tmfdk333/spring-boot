4c36994b1dffcbe6c839bcded88e5696d9145a62
tmfdk333 <tmfdk333@gmail.com>
2019-03-13 17:43:35 +0900

    2.4.2.3 쿠키값 수정 및 삭제

--- ch02/src/main/java/info/thecodinglive/cookie/CookieModifyServlet.java
+++ ch02/src/main/java/info/thecodinglive/cookie/CookieModifyServlet.java
@@ -9,23 +9,25 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns={"/readcookie"})
-public class CookieReadServlet extends HttpServlet {
+@WebServlet(urlPatterns="/modicookie")
+public class CookieModifyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        out.println("<html><head><title>쿠키 읽기</title></head><body>");
+        out.println("<html><head><title>cookie 수정</title></head>");
+        out.println("<body>");
         Cookie[] cookies = req.getCookies();
+
         if (cookies != null) {
             for (Cookie cookie : cookies) {
                 if (cookie.getName().equals("jpub")) {
-                    out.println("cookie::" + cookie.getValue());
+                    Cookie modifiedCookie = new Cookie("jpub", "read");
+                    resp.addCookie(modifiedCookie);
                 }
             }
         }
-        out.println("<a href='/modicookie'>쿠키수정</a></body></html>");
-        out.println("</body></html>");
+        out.println("<a href='/readcookie'>readcookie</a></body></html>");
     }
 }
