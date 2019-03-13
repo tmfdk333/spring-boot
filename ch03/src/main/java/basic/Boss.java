7d9f4c2073dafb3572cc79ec7786932431119161
tmfdk333 <tmfdk333@gmail.com>
2019-03-13 22:53:05 +0900

    3.2.3.3 어노테이션 설정 시 생명주기 제어

--- ch03/src/main/java/basic/Boss.java
+++ ch03/src/main/java/basic/Boss.java
@@ -1,13 +1,20 @@
 package basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class Boss implements WorkManager {
     @Override
     public String doIt() {
         return "do Boss";
     }
+
+    @PostConstruct
     public void onCreated() {
         System.out.println("Boss 생성");
     }
+
+    @PreDestroy
     public void onDestroyed() {
         System.out.println("Boss 소멸");
     }
